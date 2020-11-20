a4c8be75b77b81a5e4101910bca683272be912dc
FL45h-09 <vishal.ravanank@gmail.com>
2020-11-20 18:41:34 +0530

    changes in change-pass and error and new changeP file.

--- change_pass.php
+++ change_pass.php
@@ -20,31 +20,47 @@
 <!-- Body starts here... -->
 
 <div class="container mx-auto px-8 my-8 md:flex h-screen items-center justify-center">
-<form class="w-full max-w-lg">
+<form method="POST" action="/includes/changeP.php" class="w-full max-w-lg">
 
 <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full px-3">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
         Current Password
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
-      <p class="text-gray-600 text-xs italic">Please type your current password.</p>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" name="cPasswd" type="password" placeholder="******************">
+      <?php
+        if(isset($_SESSION["error"]))
+        {
+          include("includes/error.php");
+          unset($_SESSION["error"]);
+        }else{echo '<p class="text-gray-600 text-xs italic">Please type your current password.</p>';}
+      ?>
+      
     </div>
   </div>
+
   <div class="flex flex-wrap -mx-3 mb-6">
 
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
         New Password
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="password" placeholder="New Password">
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="password" name="nPasswd1" placeholder="New Password">
     </div>
     <div class="w-full md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
         Retype Password
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="password" placeholder="New password">
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="password" name="nPasswd2" placeholder="New password">
     </div>
+
+     <?php
+        if(isset($_SESSION["error2"]))
+        {
+          include("includes/error.php");
+          unset($_SESSION["error2"]);
+        }
+      ?>
   </div>
 
   <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
