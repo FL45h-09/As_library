6d6a677718ab33e9543720a82b803e25d1fc55e8
FL45h-09 <vishal.ravanank@gmail.com>
2020-11-23 09:00:48 +0530

    email login added to verify.php

--- includes/verify.php
+++ includes/verify.php
@@ -7,30 +7,65 @@
 
         if(count($_POST)>0)
         {
-        $sqlQ = "SELECT * FROM users WHERE userid = '$usrID'";
+        //Check if the given userid input is email or userID.
+            if (filter_var($usrID, FILTER_VALIDATE_EMAIL)) {
 
-    $reslt = mysqli_query($conn, $sqlQ);
+                $sqlQ = "SELECT * FROM users WHERE email = '$usrID'";
+                $reslt = mysqli_query($conn, $sqlQ);
 
-    while($row = mysqli_fetch_array($reslt)){
-      
-            $dbpass = $row['passwd']; 
-            $dbusr = $row['userid']; 
-    }
+                while($row = mysqli_fetch_array($reslt)){
+                
+                        $dbpass = $row['passwd'];
+                        $dbmail =   $row['email'];
+                        $dbusr = $row['userid'];
+                }
 
-    if($usrID == $dbusr AND $passwd == $dbpass)
-    {
-        $_SESSION["user_n"] = $dbusr;
+                if($usrID == $dbmail AND $passwd == $dbpass)
+                {
+                    $_SESSION["user_n"] = $dbusr;
 
-        if(isset($_SESSION["user_n"]))
-        {
-            $conn->close();
-            header("Location:/index.php");
-        }
-    }
-    else
-    {
-        $_SESSION["error"] = "Invalid username or password.";
-        header("Location:/login.php");
-    }
+                    if(isset($_SESSION["user_n"]))
+                    {
+                        $conn->close();
+                        header("Location:/index.php");
+                    }
+                }
+                else
+                {
+                    $_SESSION["error"] = "Invalid username or password.";
+                    $conn->close();
+                    header("Location:/login.php");
+                }
+                        
+
+            } else {
+
+                $sqlQ = "SELECT * FROM users WHERE userid = '$usrID'";
+                $reslt = mysqli_query($conn, $sqlQ);
+
+                while($row = mysqli_fetch_array($reslt)){
+                
+                        $dbpass = $row['passwd'];
+                        $dbusr = $row['userid'];
+                }
+
+                if($usrID == $dbusr AND $passwd == $dbpass)
+                {
+                    $_SESSION["user_n"] = $dbusr;
+
+                    if(isset($_SESSION["user_n"]))
+                    {
+                        $conn->close();
+                        header("Location:/index.php");
+                    }
+                }
+                else
+                {
+                    $_SESSION["error"] = "Invalid username or password.";
+                    $conn->close();
+                    header("Location:/login.php");
+                }
+            }
+
     }
 ?>
--- test2.php
+++ test2.php
@@ -1,6 +1,6 @@
 <?php
 
-    $email = array("test", "test2", "test@example.com", "test2@example.com", "test392", "test@example.net", "vishal", "vishal@gmail.com");
+    $email = array("test", "test2", "test@example.com", "test2@example.com", "test392", "test@example.net", "Sanker", "vishal@gmail.com", 3, 5);
 
     function checkEmail($email) {
         $find1 = strpos($email, '@');
@@ -12,7 +12,17 @@
 
     foreach($email as $value){
         if ( checkEmail($value) ) {
-            echo $value . ' ( looks like a valid email address.)'."</br>";
-        }
+            echo "</br>".$value . ' ( looks like a valid email address.)'."</br>";
+        }else {
+        echo("</br>$value is not a valid email address </br>");
+    }
+    }
+
+    foreach($email as $value){
+    if (filter_var($value, FILTER_VALIDATE_EMAIL)) {
+        echo("</br>$value is a valid email address");
+    } else {
+        echo("</br>$value is not a valid email address");
+    }
     }
 ?>
