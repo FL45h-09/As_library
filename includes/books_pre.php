66841c4e9d2b27207322f73b72ad9a28c82444ff
FL45h-09 <vishal.ravanank@gmail.com>
2020-11-29 15:17:05 +0530

    Modification in notify and search bar with new css added in style.css

--- includes/books_pre.php
+++ includes/books_pre.php
@@ -1,148 +1,147 @@
 <?php
-  //include("includes/sessions.php");
-?>  
-  
-  <section class="text-gray-700 body-font ">
-    <div class="container px-5 py-20 mx-auto ">
-      <div class="flex flex-wrap -m-4 ">
+//include("includes/sessions.php");
+?>
+
+<section class="text-gray-700 body-font ">
+  <div class="container px-5 py-20 mx-auto ">
+    <div class="flex flex-wrap -m-4 ">
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\avengers.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Avengers</h2>
-          
+
         </div>
       </div>
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\spidy.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Spider Man</h2>
-          
+
         </div>
       </div>
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\civil_war.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Civil War</h2>
-          
+
         </div>
       </div>
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\captain_marvel.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Captain Marvel</h2>
-          
+
         </div>
       </div>
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a1.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Lab Girl</h2>
-         
+
         </div>
       </div>
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a2.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">The Glass Universe</h2>
-          
+
         </div>
       </div>
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a3.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">The Gene</h2>
-          
+
         </div>
       </div>
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a5.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Eruption</h2>
-         
+
         </div>
       </div>
 
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a4.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">What The F</h2>
-          
+
         </div>
       </div>
 
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a6.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Seven Skeletons</h2>
-          
+
         </div>
       </div>
 
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a8.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Silent Sparks</h2>
-          
+
         </div>
       </div>
 
       <div class="lg:w-1/6 md:w-1/2 p-4 w-full">
-        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 modal-open">
+        <a href="#" class="block relative h-48 rounded overflow-hidden border-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl hover:scale-110 modal-open">
           <img alt="Book" class="object-cover object-center w-full h-full block" src="\imgs\a10.jpg">
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Name</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">Slick Water</h2>
-          
+
         </div>
       </div>
-      
+
     </div>
   </div>
 
-<!--The following include is to add modal in this page -->
+  <!--The following include is to add modal in this page -->
   <?php
-    if($mod2 == 1){
+  if ($mod2 == 1) {
     include("includes/modal.php");
-    }elseif($mod2 == 0){
-      include("includes/modal2.php");
-    }
+  } elseif ($mod2 == 0) {
+    include("includes/modal2.php");
+  }
   ?>
-<!--This is the JavaScript for the mpodal to work properly... -->
+  <!--This is the JavaScript for the mpodal to work properly... -->
   <script src="\js\main.js"></script>
 
 </section>
-
